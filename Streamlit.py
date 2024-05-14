7e10b8c3b3633ce56b422d3c5eab6edcb5e3eadf
luanappsilvaa <168466309+luanappsilvaa@users.noreply.github.com>
2024-05-14 14:31:46 -0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,10 +1,35 @@
-# app.py
-import streamlit as st
+import random
 
-def main():
-    st.title("Meu Programa Streamlit")
-    st.write("Olá! Este é um programa simples no estilo GitHub.")
-    st.write("Espero que goste!")
+def jogar_adedonha():
+    categorias = {
+        'A': 'Nome próprio',
+        'B': 'País',
+        'C': 'Fruta',
+        'D': 'Animal',
+        'E': 'Cor',
+        # Adicione mais categorias conforme desejado
+    }
+
+    jogador = input("Digite o nome do jogador: ")
+    rodadas = int(input("Quantas rodadas você deseja jogar? "))
+
+    pontos_jogador = 0
+
+    for _ in range(rodadas):
+        letra = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        print("\nLetra sorteada:", letra)
+
+        for categoria_letra, categoria_descricao in categorias.items():
+            resposta = input(f"Digite uma {categoria_descricao} que comece com a letra {letra}: ").capitalize()
+
+            if resposta.startswith(letra):
+                pontos_jogador += 1
+                print("Resposta válida! +1 ponto!")
+            else:
+                print("Resposta inválida! 0 pontos.")
+
+    print(f"\nO jogador {jogador} fez {pontos_jogador} pontos em {rodadas} rodadas.")
 
 if __name__ == "__main__":
-    main()
+    print("Bem-vindo ao Jogo da Adedonha!")
+    jogar_adedonha()
