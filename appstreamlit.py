43ae8390449344c4428595e3b144c9a44dcc8912
luanappsilvaa <168466309+luanappsilvaa@users.noreply.github.com>
2024-05-17 10:24:33 -0300

    Update appstreamlit.py

--- appstreamlit.py
+++ appstreamlit.py
@@ -1,7 +1,9 @@
+import streamlit as st
 import random
 
 def jogar_adedonha():
-    print("Bem-vindo ao jogo da Adedonha!")
+    st.title("Jogo da Adedonha")
+
     categorias = {
         "Nome": ["Alice", "Bruno", "Carla", "Daniel", "Eva"],
         "Animal": ["Cachorro", "Gato", "Elefante", "Pássaro", "Cobra"],
@@ -9,43 +11,39 @@ def jogar_adedonha():
         "Cor": ["Azul", "Vermelho", "Verde", "Amarelo", "Roxo"],
         "Minha sogra é": ["Paciente", "Engraçada", "Trabalhadora", "Carinhosa", "Inteligente"]
     }
-    
-    categoria = random.choice(list(categorias.keys()))
-    palavra = random.choice(categorias[categoria])
-    
-    print(f"A categoria escolhida é: {categoria}")
-    print("A palavra tem", len(palavra), "letras.")
-    
-    tentativas = 3
-    letras_descobertas = []
-    for letra in palavra:
-        letras_descobertas.append("_")
-    
-    while tentativas > 0:
-        print("Palavra atual:", " ".join(letras_descobertas))
-        palpite = st.text_input("Digite uma letra ou a palavra completa: ").upper()
-        
+
+    categoria_escolhida = st.selectbox("Escolha uma categoria:", list(categorias.keys()))
+
+    palavra = random.choice(categorias[categoria_escolhida])
+
+    st.write(f"A palavra escolhida é da categoria '{categoria_escolhida}':")
+    st.write("_ " * len(palavra))
+
+    tentativas_restantes = 3
+    letras_descobertas = ["_"] * len(palavra)
+
+    while tentativas_restantes > 0 and "_" in letras_descobertas:
+        palpite = st.text_input("Digite uma letra ou a palavra completa:").upper()
+
         if len(palpite) == 1:
             if palpite in palavra:
-                print("Letra correta!")
+                st.write("Letra correta!")
                 for i in range(len(palavra)):
                     if palavra[i] == palpite:
                         letras_descobertas[i] = palpite
             else:
-                print("Letra incorreta!")
-                tentativas -= 1
+                st.write("Letra incorreta!")
+                tentativas_restantes -= 1
         elif palpite == palavra:
-            print("Parabéns! Você acertou a palavra!")
+            st.write("Parabéns! Você acertou a palavra!")
             break
         else:
-            print("Palavra incorreta!")
-            tentativas -= 1
-        
-        if "_" not in letras_descobertas:
-            print("Parabéns! Você descobriu a palavra!")
-            break
-    
-    if tentativas == 0:
-        print("Suas tentativas acabaram. A palavra era:", palavra)
+            st.write("Palavra incorreta!")
+            tentativas_restantes -= 1
+
+    if "_" not in letras_descobertas:
+        st.write("Parabéns! Você descobriu a palavra!")
+    else:
+        st.write("Suas tentativas acabaram. A palavra era:", palavra)
 
 jogar_adedonha()
